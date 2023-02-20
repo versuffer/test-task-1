778e3c62e26bca1a51697e546ca9d48180469f57
versuffer <versuffer@mail.ru>
2023-02-20 20:27:24 +0300

    Initialized employee model

--- tree_loader/models.py
+++ tree_loader/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    date_of_employment = models.DateField()
+    salary = models.FloatField()
+    supervisor_id = models.PositiveBigIntegerField()
+
